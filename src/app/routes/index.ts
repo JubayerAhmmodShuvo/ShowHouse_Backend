aed380e05ee7b27b5154283dab69cd3e39a5dfcd
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-11-18 11:45:51 +0600

    product work done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { UserRoutes } from '../modules/user/users.routes';
 import { UserAuth } from '../modules/auth/auth.route';
+import { ProductRoutes } from '../modules/product/product.routes';
 
 
 
@@ -15,8 +16,10 @@ const moduleRoutes = [
     path: '/auth',
     route: UserAuth,
   },
-
-  
+  {
+    path: '/product',
+    route: ProductRoutes,
+  },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
